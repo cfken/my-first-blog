bfd4d335cabbea2c8d06a18304190250c9d0ebf1
albrightke <cfken@yahoo.com>
2016-10-20 11:41:21 -0700

    edits

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-from django.shortcuts import redirect
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()
@@ -16,7 +17,6 @@ def post_detail(request, pk):
     
 def post_new(request):
     if request.method == "POST":
-        #form = PostForm(instance=post)
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
@@ -24,9 +24,9 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-                form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
         
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
